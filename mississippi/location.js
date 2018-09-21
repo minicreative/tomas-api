079d02d502c212e61188e612b10b9284a193222c
Tomas Roy <hello@minicreative.net>
2018-09-21 11:00:28 -0400

    geocoder replaced with get request

--- mississippi/location.js
+++ mississippi/location.js
@@ -3,10 +3,11 @@
 let NumberFilter = new RegExp("^[0-9.-]*$");
 
 // Initialize dependencies
+const Config = require("./../config");
 const Mongoose = require("mongoose");
 const Schema = Mongoose.Schema;
 const Moment = require("moment");
-const Geocoder = require("geocoder");
+const Request = require("request");
 
 // Initialize model
 let Location = Mongoose.model("Location", new Schema({
@@ -43,16 +44,21 @@ module.exports = {
         if (note) locationDetails.note = note;
 
         // Geocode location
-        Geocoder.reverseGeocode(latitude, longitude, function (err, data) {
+		let reverseGeocodeEndpoint = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+		reverseGeocodeEndpoint += latitude + "," + longitude;
+		reverseGeocodeEndpoint += "&key=" + Config.MAPS_AUTH;
+        Request(reverseGeocodeEndpoint, function (err, response, body) {
 
             // Handle error
-            if (err) locationDetails.name = latitude + " / " + longitude;
+            if (err || !body) 
+				locationDetails.name = latitude + " / " + longitude;
 
             // Handle success
             else {
-                var place = data.results[0];
-                var locationString = "";
-                for (var i in place.address_components) {
+				let results = JSON.parse(body).results;
+                let place = results[0];
+                let locationString = "";
+				for (var i in place.address_components) {
                     if (place.address_components[i].types[0] == "locality") 
                         locationString += place.address_components[i].long_name;
                     if (place.address_components[i].types[0] == "administrative_area_level_1")
